a0f591626745aa5f406ba70205d5684f04e8bab6
VEX <72231218+ahmedSherif-eng@users.noreply.github.com>
2024-01-27 22:06:54 +0200

    "..."

--- SeamCarver.java
+++ SeamCarver.java
@@ -16,14 +16,14 @@ public class SeamCarver {
     public SeamCarver(Picture picture) {
         if (picture == null)
             throw new IllegalArgumentException();
-        this.picture = picture;
+        this.picture = new Picture(picture);
         col = width();
         row = height();
         Energy = new double[col][row];
         distanceTo = new double[col][row];
         for (int i = 0; i < row; i++)
             for (int j = 0; j < col; j++)
-                Energy[j][i] = -1;
+                Energy[j][i] = Double.POSITIVE_INFINITY;
         initEnergy();
     }
 
@@ -60,7 +60,7 @@ public class SeamCarver {
     public double energy(int x, int y) {
         if (x >= col || y >= row)
             throw new IllegalArgumentException();
-        if (Energy[x][y] == -1) {
+        if (Energy[x][y] == Double.POSITIVE_INFINITY) {
             double deltaX = getDeltaX(x, y);
             double deltaY = getDeltaY(x, y);
             double energy = Math.sqrt(deltaX + deltaY);
@@ -110,17 +110,26 @@ public class SeamCarver {
      *           (x-1,y+1)
      * */
     public int[] findHorizontalSeam() {
+    /*    transpose();
+        picture.save("xx.png");
+        int[] seam = findVerticalSeam();
+        // transpose();
+        return seam;*/
         Picture transposed = new Picture(picture.height(), picture.width());
         for (int x = 0; x < col; x++)
             for (int y = 0; y < row; y++)
                 transposed.set(y, x, picture.get(x, y));
-        transposed.save("xx.png");
         SeamCarver flipped = new SeamCarver(transposed);
         return flipped.findVerticalSeam();
-
-
     }
 
+    private void transpose() {
+        Picture transposed = new Picture(picture.height(), picture.width());
+        for (int x = 0; x < picture.width(); x++)
+            for (int y = 0; y < picture.height(); y++)
+                transposed.set(y, x, picture.get(x, y));
+        picture = transposed;
+    }
     // Function to manually transpose the picture
 
     private void relax(int x, int y) {
@@ -153,28 +162,111 @@ public class SeamCarver {
             for (x = 1; x < col - 1; x++)
                 relax(x, y);
         double min = Double.POSITIVE_INFINITY;
-        for (int j = 1; j < row; j++) {
-            for (i = 0; i < col; i++)
-                if (distanceTo[i][j] < min) {
-                    min = distanceTo[i][j];
-                    seam[j] = i;
-                    if (j == 1)
-                        seam[0] = i;
+        for (i = 1; i < col; i++)
+            if (distanceTo[i][1] < min) {
+                min = distanceTo[i][1];
+                seam[1] = i;
+                seam[0] = i;
+            }
+        int minimumIndexCol = seam[1]; // column
+        for (i = 2; i < row; i++) {
+            if (minimumIndexCol + 1 < col) {
+                if (minimumIndexCol - 1 >= 0) {
+                    if (distanceTo[minimumIndexCol][i] < distanceTo[minimumIndexCol - 1][i]
+                            && distanceTo[minimumIndexCol][i] < distanceTo[minimumIndexCol + 1][i])
+                        seam[i] = minimumIndexCol;
+
+                    else if (distanceTo[minimumIndexCol + 1][i] < distanceTo[minimumIndexCol][i]
+                            && distanceTo[minimumIndexCol + 1][i] < distanceTo[minimumIndexCol
+                            - 1][i])
+                        seam[i] = minimumIndexCol + 1;
+                    else if (distanceTo[minimumIndexCol - 1][i] < distanceTo[minimumIndexCol + 1][i]
+                            && distanceTo[minimumIndexCol - 1][i] < distanceTo[minimumIndexCol][i])
+                        seam[i] = minimumIndexCol - 1;
                 }
+                else {
+                    if (distanceTo[minimumIndexCol][i] < distanceTo[minimumIndexCol + 1][i])
+                        seam[i] = minimumIndexCol;
+
+                    else if (distanceTo[minimumIndexCol + 1][i] < distanceTo[minimumIndexCol][i])
+                        seam[i] = minimumIndexCol + 1;
+                }
+            }
+            else {
+                if (distanceTo[minimumIndexCol][i] < distanceTo[minimumIndexCol - 1][i])
+                    seam[i] = minimumIndexCol;
+                else if (distanceTo[minimumIndexCol - 1][i] < distanceTo[minimumIndexCol][i])
+                    seam[i] = minimumIndexCol - 1;
+            }
+            minimumIndexCol = seam[i];
         }
+
+
+     /*   for (i = 0; i < row; i++)
+            System.out.println(seam[i] + "  ");*/
         return seam;
     }
 
     // remove horizontal seam from current picture
     public void removeHorizontalSeam(int[] seam) {
-        if (seam == null || seam.length > row)
-            throw new IllegalArgumentException();
+        if (seam == null || seam.length != width() || height() <= 1)
+            throw new IllegalArgumentException("Invalid seam");
+
+        Picture newPicture = new Picture(width(), height() - 1);
+        for (int x = 0; x < width(); x++) {
+            for (int y = 0; y < height() - 1; y++) {
+                if (y < seam[x]) {
+                    newPicture.setRGB(x, y, picture.getRGB(x, y));
+                }
+                else {
+                    newPicture.setRGB(x, y, picture.getRGB(x, y + 1));
+                }
+            }
+        }
+        picture = newPicture;
+        // Recalculate energy for affected pixels
+        updateEnergyAfterRemoval(seam);
     }
 
     // remove vertical seam from current picture
     public void removeVerticalSeam(int[] seam) {
-        if (seam == null || seam.length > row)
-            throw new IllegalArgumentException();
+        if (seam == null || seam.length != height() || width() <= 1)
+            throw new IllegalArgumentException("Invalid seam");
+
+        Picture newPicture = new Picture(width() - 1, height());
+        for (int y = 0; y < height(); y++) {
+            for (int x = 0; x < width() - 1; x++) {
+                if (x < seam[y]) {
+                    newPicture.setRGB(x, y, picture.getRGB(x, y));
+                }
+                else {
+                    newPicture.setRGB(x, y, picture.getRGB(x + 1, y));
+                }
+            }
+        }
+        picture = newPicture;
+        // Recalculate energy for affected pixels
+        updateEnergyAfterRemoval(seam);
+    }
+
+    // Recalculate energy values for pixels affected by seam removal
+    private void updateEnergyAfterRemoval(int[] seam) {
+        double[][] newEnergy = new double[width()][height()];
+
+        for (int y = 0; y < height(); y++) {
+            int k = 0;
+            for (int x = 0; x < width(); x++) {
+                if (x != seam[y]) {
+                    Energy[x][y] = energy(x, y);
+                    newEnergy[k][y] = Energy[x][y];
+                    k++;
+                }
+            }
+        }
+
+        Energy = newEnergy;
+        col = width();
+        row = height();
     }
 
     private void printEnergy() {
@@ -185,9 +277,18 @@ public class SeamCarver {
         }
     }
 
+    private void printDistanceTo() {
+        for (int r = 0; r < row; r++) {
+            for (int c = 0; c < col; c++)
+                System.out.print(distanceTo[c][r] + " ");
+            System.out.print("\n");
+        }
+    }
+
     public static void main(String[] args) {
         SeamCarver obj = new SeamCarver(new Picture("HJocean.png"));
         obj.findHorizontalSeam();
+        obj.printDistanceTo();
 
 
     }
